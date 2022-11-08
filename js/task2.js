b0b6e95b69b920fd4e64668dba6ceed20b9f510d
Assylaiym Kairatova <assylaiym.kairatova@gmail.com>
2022-11-08 12:57:33 +0600

    Добавлен task2.js

---
+++ js/task2.js
@@ -0,0 +1,9 @@
+
+const elements = document.querySelectorAll(".element");
+
+for (i = 0; i < elements.length; i++) {
+elements[i].style.color = "red";
+if(i>2){
+		elements[i].style.color = "green";
+        }
+}
